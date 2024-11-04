1018047ae6ef81c650db6387c414d0acb69515f1
adnan-mkhtr <adnanmuchtar8e01@gmail.com>
2024-11-04 15:09:48 +0700

    Fix lab7 kowan

--- rectangular.py
+++ rectangular.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-import requests
+import requests, os
 
 app = Flask(__name__)
 
@@ -60,4 +60,5 @@ def calculate_cube():
 
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="127.0.0.1", port=port)
