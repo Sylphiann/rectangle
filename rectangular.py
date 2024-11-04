1cfc8ad24f36bd96337934707dfa8743edc8b5a2
Sylphiann <hydrolilium@gmail.com>
2024-11-04 13:24:08 +0700

    Add another form and improve UX

--- rectangular.py
+++ rectangular.py
@@ -10,26 +10,41 @@ def get_square_area(num):
     result = round(result['luas'], 2)
     return result
 
-
-@app.route('/',  methods=['GET'])
+@app.route('/', methods=['GET'])
 def home():
-    return render_template('index.html')
+    return render_template('index.html', res1=None, res2=None, num1="", num2="")
 
-
-@app.route('/', methods=['POST'])
-def getnum():
-    sisi = request.form.get('number')
-
-    if sisi:
+@app.route('/square', methods=['POST'])
+def calculate_square():
+    num1 = request.form.get('num1', "")
+    num2 = request.form.get('num2', "")
+    res2 = request.form.get('res2', "")
+    
+    if num1:
         try:
-            result = get_square_area(sisi)
+            res1 = get_square_area(num1)
         except ValueError:
-            result = "Invalid input"
+            res1 = "Invalid input"
     else:
-        result = "Please enter a number"
+        res1 = "Please enter a number"
+    
+    return render_template('index.html', res1=res1, res2=res2, num1=num1, num2=num2)
 
-    return render_template('index.html', result=result)
-
+@app.route('/cube', methods=['POST'])
+def calculate_cube():
+    num1 = request.form.get('num1', "")
+    num2 = request.form.get('num2', "")
+    res1 = request.form.get('res1', "")
+    
+    if num2:
+        try:
+            res2 = get_square_area(num2)
+        except ValueError:
+            res2 = "Invalid input"
+    else:
+        res2 = "Please enter a number"
+    
+    return render_template('index.html', res1=res1, res2=res2, num1=num1, num2=num2)
 
 if __name__ == '__main__':
     app.run()
