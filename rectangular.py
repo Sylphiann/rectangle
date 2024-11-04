d8fcebcf899ece261c83a79b355d8a2abb49ffde
adnan-mkhtr <adnanmuchtar8e01@gmail.com>
2024-11-04 14:26:24 +0700

    Fix lab7 kowan

--- rectangular.py
+++ rectangular.py
@@ -3,23 +3,34 @@ import requests
 
 app = Flask(__name__)
 
+
 def get_square_area(num):
-    url = f"http://35.193.138.18:8080/function/area?sisi={num}"
+    url = f"http://18.234.147.199:8080/function/area?sisi={num}"
     response = requests.get(url)
     result = response.json()
-    result = round(result['luas'], 2)
+    result = round(result["luas"], 2)
     return result
 
-@app.route('/', methods=['GET'])
-def home():
-    return render_template('index.html', res1=None, res2=None, num1="", num2="")
 
-@app.route('/square', methods=['POST'])
+def get_cube_area(num):
+    url = f"http://54.90.162.45:8080/function/area?sisi={num}"
+    response = requests.get(url)
+    result = response.json()
+    result = round(result["luas"], 2)
+    return result
+
+
+@app.route("/", methods=["GET"])
+def home():
+    return render_template("index.html", res1=None, res2=None, num1="", num2="")
+
+
+@app.route("/square", methods=["POST"])
 def calculate_square():
-    num1 = request.form.get('num1', "")
-    num2 = request.form.get('num2', "")
-    res2 = request.form.get('res2', "")
-    
+    num1 = request.form.get("num1", "")
+    num2 = request.form.get("num2", "")
+    res2 = request.form.get("res2", "")
+
     if num1:
         try:
             res1 = get_square_area(num1)
@@ -27,24 +38,26 @@ def calculate_square():
             res1 = "Invalid input"
     else:
         res1 = "Please enter a number"
-    
-    return render_template('index.html', res1=res1, res2=res2, num1=num1, num2=num2)
 
-@app.route('/cube', methods=['POST'])
+    return render_template("index.html", res1=res1, res2=res2, num1=num1, num2=num2)
+
+
+@app.route("/cube", methods=["POST"])
 def calculate_cube():
-    num1 = request.form.get('num1', "")
-    num2 = request.form.get('num2', "")
-    res1 = request.form.get('res1', "")
-    
+    num1 = request.form.get("num1", "")
+    num2 = request.form.get("num2", "")
+    res1 = request.form.get("res1", "")
+
     if num2:
         try:
-            res2 = get_square_area(num2)
+            res2 = get_cube_area(num2)
         except ValueError:
             res2 = "Invalid input"
     else:
         res2 = "Please enter a number"
-    
-    return render_template('index.html', res1=res1, res2=res2, num1=num1, num2=num2)
 
-if __name__ == '__main__':
+    return render_template("index.html", res1=res1, res2=res2, num1=num1, num2=num2)
+
+
+if __name__ == "__main__":
     app.run()
